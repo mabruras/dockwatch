9c09cc210954bceaf1891ad17696e9c095242b1d
Magnus Brurås <mabruraas@gmail.com>
2019-02-05 22:04:10 +0100

    Created Flask app for finding images of running containers, and details of containers of a specific image

---
+++ app.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python3
+import json
+
+import docker
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+
+@app.route('/images', methods=['GET'])
+def get_applications():
+    client = docker.from_env()
+
+    print()
+    result = [
+        {
+            'id': container.image.id.split(':')[-1],
+            'image': get_image_name(container.image),
+            'versions': get_image_versions(container.image)
+        } for container in client.containers.list()
+    ]
+
+    client.close()
+    return Response(
+        response=json.dumps(result),
+        mimetype='application/json',
+        status=200,
+    )
+
+
+@app.route('/images/<img_id>', methods=['GET'])
+def get_app_versions(img_id):
+    client = docker.from_env()
+
+    result = [
+        {
+            'id': container.id,
+            'name': container.name,
+            'status': container.status,
+            'labels': container.labels,
+            'restarted': container.attrs.get('RestartCount', 0),
+            'state': container.attrs.get('State', dict()),
+            'created': container.attrs.get('Created', None),
+        } for container in client.containers.list()
+        if img_id in container.image.id
+    ]
+
+    client.close()
+    return Response(
+        response=json.dumps(result),
+        mimetype='application/json',
+        status=200,
+    )
+
+
+def get_image_name(img):
+    return img.tags[0].split(':')[0]
+
+
+def get_image_versions(img):
+    return [t.split(':')[-1] for t in img.tags]
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
