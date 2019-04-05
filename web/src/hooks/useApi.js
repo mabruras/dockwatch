67364272c99f3496f406535ee7ca8452196d377c
Espen <espen@gullskatten.no>
2019-04-05 14:47:05 +0200

    Nodes -> Images -> Containers -> Container Details. New flow!

--- web/src/hooks/useApi.js
+++ web/src/hooks/useApi.js
@@ -31,7 +31,7 @@ export default function useApi(opts) {
       };
       
       const res = await fetch(
-        `http://localhost:1609/api/${opts.endpoint}`,
+        `/api/${opts.endpoint}`,
         {
           ...headers,
           method: opts.method || 'GET',
